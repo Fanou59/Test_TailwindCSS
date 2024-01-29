d5d8a9a556949b1c73d9880b0efeb64be774808f
Ste[PH]ane <steph.guery@gmail.com>
2024-01-29 08:23:57 +0100

    1st

---
+++ src/app.js
@@ -0,0 +1,6 @@
+const enLirePlus = document.querySelector("#lirePlus");
+const test = document.querySelector(".test");
+
+enLirePlus.addEventListener("click", () => {
+  test.innerHTML = "Test réussi";
+});
